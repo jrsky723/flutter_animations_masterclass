7ba47b106f80609d9c10c30391b31aa4d6337fcd
hyunmo <jrsky723@gmail.com>
2024-02-19 13:50:42 +0900

    # 1.1 Implicitly Animated Widgets
    - animation: transition of a widget from one state to another
    - Implicit: 암시적인, 명시적인 애니메이션을 사용하지 않아도 애니메이션이 적용된다.
    - Implicitly Animated Widgets: 애니메이션을 직접만들지 않고, 애니메이션을 적용할 수 있는 위젯
      - Animated~가 붙은 위젯들은, Implicitly Animated Widgets이다.
      - AnimtatedOpacity
    - 여러 애니메이션을 그룹화해서 사용할 수 있다.

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ImplicitAnimationsScreen extends StatefulWidget {
   const ImplicitAnimationsScreen({super.key});
@@ -9,8 +11,48 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = false;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Implicit Animations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedAlign(
+              duration: const Duration(seconds: 1),
+              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+              child: AnimatedOpacity(
+                duration: const Duration(seconds: 1),
+                opacity: _visible ? 1 : 0,
+                child: Container(
+                  width: size.width * 0.8,
+                  height: size.width * 0.8,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: _trigger,
+              child: const Text('Go!'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
