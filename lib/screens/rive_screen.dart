575b45085ba40f60b79ec0085584b606a91325e7
hanu607 <jrsky723@gmail.com>
2024-02-26 19:23:11 +0900

    # 6.7 Conclusions
    - 텍스트는 Flutter에서, 애니메이션은 Rive에서 관리한다. (만약 애니메이션화 하지 않는다면)

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,62 +1,31 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
 class RiveScreen extends StatefulWidget {
   const RiveScreen({super.key});
-
   @override
   State<RiveScreen> createState() => _RiveScreenState();
 }
 
 class _RiveScreenState extends State<RiveScreen> {
-  late final StateMachineController _stateMachineController;
-
-  void _onInit(Artboard artboard) {
-    _stateMachineController = StateMachineController.fromArtboard(
-      artboard,
-      'state',
-      onStateChange: (stateMachineName, stateName) {
-        print('State Machine: $stateMachineName, State: $stateName');
-      },
-    )!;
-    artboard.addController(_stateMachineController);
-  }
-
-  void _togglePanel() {
-    final input = _stateMachineController.findInput<bool>("panelActive")!;
-    input.change(!input.value);
-  }
-
-  @override
-  void dispose() {
-    _stateMachineController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Stack(
         children: [
-          const RiveAnimation.asset(
-            "assets/animations/balls-animation.riv",
-            fit: BoxFit.cover,
+          RiveAnimation.asset(
+            "assets/animations/custom-button-animation.riv",
+            stateMachines: ["state"],
           ),
-          Positioned.fill(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
-              child: const Center(
-                child: Text(
-                  "Welcome to AI APP",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
+          Center(
+            child: Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
               ),
             ),
-          )
+          ),
         ],
       ),
     );
