ba9d1bdf47c4f7e63a5a0cebd4f0f2fd194c65c7
hanu607 <jrsky723@gmail.com>
2024-02-26 19:02:32 +0900

    # 6.5 Animation Blur

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -36,19 +38,26 @@ class _RiveScreenState extends State<RiveScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rive'),
-      ),
-      body: Center(
-        child: Container(
-          color: const Color(0xFFFF2ECC),
-          child: RiveAnimation.asset(
-            'assets/animations/stars-animation.riv',
-            artboard: 'artboard',
-            stateMachines: const ['state'],
-            onInit: _onInit,
+      body: Stack(
+        children: [
+          const RiveAnimation.asset(
+            "assets/animations/balls-animation.riv",
+            fit: BoxFit.cover,
           ),
-        ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: const Center(
+                child: Text(
+                  "Welcome to AI APP",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
