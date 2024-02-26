f79c6cd5b7db4a2cdf82f07358766e852e816caf
hanu607 <jrsky723@gmail.com>
2024-02-26 18:24:21 +0900

    # 6.3 State Changes
    - onStateChange: state가 변경될때, 호출된다.
    - StateMachineController에 onStateChange를 등록한다.

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -15,13 +15,15 @@ class _RiveScreenState extends State<RiveScreen> {
     _stateMachineController = StateMachineController.fromArtboard(
       artboard,
       'state',
+      onStateChange: (stateMachineName, stateName) {
+        print('State Machine: $stateMachineName, State: $stateName');
+      },
     )!;
     artboard.addController(_stateMachineController);
   }
 
   void _togglePanel() {
     final input = _stateMachineController.findInput<bool>("panelActive")!;
-
     input.change(!input.value);
   }
 
@@ -38,23 +40,14 @@ class _RiveScreenState extends State<RiveScreen> {
         title: const Text('Rive'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 400,
-              width: double.infinity,
-              child: RiveAnimation.asset(
-                'assets/animations/old-man-animation.riv',
-                artboard: 'main',
-                stateMachines: const ['state'],
-                onInit: _onInit,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: _togglePanel,
-              child: const Text('Go!'),
-            )
-          ],
+        child: Container(
+          color: const Color(0xFFFF2ECC),
+          child: RiveAnimation.asset(
+            'assets/animations/stars-animation.riv',
+            artboard: 'artboard',
+            stateMachines: const ['state'],
+            onInit: _onInit,
+          ),
         ),
       ),
     );
