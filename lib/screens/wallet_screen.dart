2291b66423bd2e351cce63fec7a93a8b767c8116
hanu607 <jrsky723@gmail.com>
2024-02-29 22:58:39 +0900

    # 8.3 Credit Cards
    - Container
      - width: double.infinity를 사용해서, 화면 전체를 차지하도록 한다.

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -15,28 +15,95 @@ class _WalletScreenState extends State<WalletScreen> {
       appBar: AppBar(
         title: const Text('Wallet'),
       ),
-      body: Center(
-        child: const Text(
-          'Hello!',
-          style: TextStyle(fontSize: 66),
-        )
-            .animate()
-            .fadeIn(
-              begin: 0,
-              duration: 5.seconds,
-            )
-            .scale(
-              alignment: Alignment.center,
-              begin: Offset.zero,
-              duration: 5.seconds,
-              end: const Offset(1, 1),
-            )
-            .then(delay: 5.seconds)
-            .slideX(
-              begin: 0,
-              end: -10,
-              duration: 2.seconds,
+      body: const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            CreditCard(bgColor: Colors.purple),
+            CreditCard(bgColor: Colors.black),
+            CreditCard(bgColor: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CreditCard extends StatelessWidget {
+  final Color bgColor;
+
+  const CreditCard({super.key, required this.bgColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: bgColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 30,
+          horizontal: 40,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Nomad Coders',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      '**** **** **** 1234',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 20,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
